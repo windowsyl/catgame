0c01efd9ffed2ae0c5e871a9a1c120dc0b6a0604
windowsyl <ericc@khanlabschool.org>
2022-03-10 09:59:51 -0800

    added sound effects, some other stuff
    not really keeping track, tried to make a cat sprite and it currently looks awful

--- sketch.js
+++ sketch.js
@@ -1,21 +1,72 @@
 let r = 20;
 let s; //r * sqrt(3)/2
 class Tile {
-  constructor(x, y, num, neighbors = []) {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
     this.darkened = false;
-    this.num = num;
-    this.neighbors = neighbors;
+
+    this.animationTimer = 0;
+    this.animationLength = 1;
+    this.initialLoc = { x: 0, y: 0 };
+    this.isFalling = false;
+  }
+  shockwave() {
+    tiles.forEach((t) => {
+      let rockDist = dist(t.x, t.y, this.x, this.y);
+      if (rockDist < 60 && rockDist > 0) {
+        t.animationTimer = 0.15;
+        t.animationLength = 0.15;
+        let xoffset = t.x + 0.3 * (t.x - this.x);
+        let yoffset = t.y + 0.3 * (t.y - this.y);
+        t.initialLoc = { x: xoffset, y: yoffset };
+
+        if (t == cat.location) {
+          catHit.play();
+        }
+      }
+    });
   }
   draw() {
+    if (this.animationTimer <= 0) {
+      this.basicDraw(this.x, this.y);
+      if (this.isFalling) {
+        this.isFalling = false;
+        this.shockwave();
+      }
+    } else {
+      if (this.isFalling) {
+        this.darkened = false;
+        this.basicDraw(this.x, this.y);
+        this.darkened = true;
+      }
+      this.basicDraw(
+        lerp(
+          this.initialLoc.x,
+          this.x,
+          1 - this.animationTimer / this.animationLength
+        ),
+        lerp(
+          this.initialLoc.y,
+          this.y,
+          1 - this.animationTimer / this.animationLength
+        )
+      );
+      this.animationTimer -= deltaTime / 1000;
+    }
+  }
+  basicDraw(x, y) {
     if (this.darkened) fill(70);
     else fill(0, 70, 0);
     noStroke();
-    hexagon(this);
-    textSize(10);
-    fill(0, 0, 0);
-    text(this.num, this.x, this.y);
+    hexagon(x, y);
+  }
+  startFalling() {
+    this.isFalling = true;
+    this.darkened = true;
+    this.initialLoc = { x: this.x, y: this.y - 60 };
+    this.animationTimer = 0.1;
+    this.animationLength = 0.1;
   }
 }
 
@@ -25,7 +76,38 @@ class Cat {
   }
   draw() {
     fill(0);
-    circle(this.location.x, this.location.y, 20);
+    let drawx = lerp(
+      this.location.initialLoc.x,
+      this.location.x,
+      1 - this.location.animationTimer / this.location.animationLength
+    );
+    let drawy = lerp(
+      this.location.initialLoc.y,
+      this.location.y,
+      1 - this.location.animationTimer / this.location.animationLength
+    );
+    this.stamp(drawx, drawy);
+  }
+  ear(x, y) {
+    fill('tan');
+    noStroke();
+    triangle(x - 8, y - 5, x - 4, y - 14, x - 1, y - 5);
+    strokeWeight(2);
+    stroke(0);
+    curve(x - 8, y - 5, x - 4, y - 14, x - 1, y - 5, x - 8, y - 5);
+    curve(x - 1, y - 5, x - 8, y - 5, x - 4, y - 14, x - 1, y - 5);
+  }
+  stamp(x, y) {
+    x -= 3;
+    y += 1;
+    fill(0);
+    stroke(0);
+    circle(x, y, 20);
+    this.ear(x, y-1);
+    this.ear(x+8,y-1)
+    noFill();
+    strokeWeight(3);
+    bezier(x + 18, y - 4, x + 7, y - 6, x + 19, y + 8, x + 5, y + 7);
   }
   get x() {
     return this.location.x;
@@ -40,10 +122,8 @@ function makeTiles() {
   let spacing = 3;
   for (let y = 25 + 2 * s; y < height - 25 - s; y += 2 * s + spacing * 2) {
     for (let x = 25 + r; x < width - 25 - r; x += 3 * r + spacing * 2) {
-      tiles.push(new Tile(x, y, tiles.length));
-      tiles.push(
-        new Tile(x + 1.5 * r + spacing, y + s + spacing, tiles.length)
-      );
+      tiles.push(new Tile(x, y));
+      tiles.push(new Tile(x + 1.5 * r + spacing, y + s + spacing));
     }
   }
   return tiles;
@@ -60,13 +140,24 @@ function mouseClicked() {
         if (minDist < s) break;
       }
     }
-    // console.log(nearest.x, nearest.y)
     if (activePlayer == 0) {
       if (!nearest.darkened && cat.location != nearest) {
-        nearest.darkened = true;
+        nearest.startFalling();
         rockPlaced.play();
-        screenShakePhase++;
         activePlayer = 1;
+        catNeighbors = [];
+        tiles.forEach((t) => {
+          let catDist = dist(t.x, t.y, cat.x, cat.y);
+          if (catDist < 60 && catDist > 0) {
+            catNeighbors.push(t);
+          }
+        });
+
+        if (catNeighbors.every((t) => t.darkened)) {
+          console.log('rocks win');
+          rocksWin.play();
+          activePlayer = 3;
+        }
       }
     } else if (activePlayer == 1) {
       if (
@@ -88,22 +179,10 @@ function mouseClicked() {
   }
 }
 
-let screenShakePhase = -1;
-function screenShake() {
-  let cx = width / 2;
-  let cy = height / 2;
-  let factor = 1.1
-  translate(cx * (factor-1), cy * (factor-1));
-  scale(factor);
-  // translate(30*sin(5*screenShakePhase-PI)/(screenShakePhase-PI/5), 0)
-  // screenShakePhase++;
-  // if(screenShakePhase > 8/5*PI) screenShakePhase = -1
-}
-
-function hexagon(tile) {
+function hexagon(x, y) {
   beginShape();
   for (let a = 0; a < TWO_PI; a += PI / 3) {
-    vertex(tile.x + cos(a) * r, tile.y + sin(a) * r);
+    vertex(x + cos(a) * r, y + sin(a) * r);
   }
   endShape(CLOSE);
 }
@@ -113,13 +192,17 @@ let cat;
 let activePlayer = 0;
 let rockPlaced;
 let catMoved;
+let catHit;
 let catWins;
+let rocksWin;
 function setup() {
   createCanvas(800, 500, P2D);
   s = (sqrt(3) / 2) * r;
   rockPlaced = loadSound('rockPlaced.m4a');
+  catHit = loadSound('catHit.m4a');
   catMoved = loadSound('catMoved.m4a');
   catWins = loadSound('catWins.m4a');
+  rocksWin = loadSound('rocksWin.m4a');
 
   tiles = makeTiles();
   cat = new Cat(tiles[120]);
@@ -131,14 +214,17 @@ function setup() {
 
 function draw() {
   background(200);
-  // if(screenShakePhase > -1) screenShake();
-  screenShake();
-  tiles.forEach((t) => { t.draw();
+  tiles.forEach((t) => {
+    t.draw();
   });
   cat.draw();
   if (activePlayer == 2) {
     textSize(120);
     fill(212, 175, 55);
     text('cat wins!!', 150, 300);
+  } else if (activePlayer == 3) {
+    textSize(120);
+    fill(212, 175, 55);
+    text('rocks win!!', 150, 300);
   }
 }
